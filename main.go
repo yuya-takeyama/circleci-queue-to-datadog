9afc8f97c544fb953ca69415d6a5b6371e33c742
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2018-09-09 05:27:35 +0900

    Add tags

--- main.go
+++ main.go
@@ -16,31 +16,96 @@ var datadogClient = datadog.NewClient(os.Getenv("DATADOG_API_KEY"), "")
 var runningMetricName = "circleci.queue.running"
 var notRunningMetricName = "circleci.queue.not_running"
 
+type jobCount struct {
+	VcsType  string
+	Username string
+	Reponame string
+	Branch   string
+	Count    int
+}
+
+type jobCounts struct {
+	jobCounts map[string]*jobCount
+}
+
+func newJobCounts() *jobCounts {
+	return &jobCounts{
+		jobCounts: make(map[string]*jobCount),
+	}
+}
+
+func (o *jobCounts) incr(job *circleCiJob) {
+	key := fmt.Sprintf("%s/%s/%s/%s", job.VcsType, job.Username, job.Reponame, job.Branch)
+	if jc, ok := o.jobCounts[key]; ok {
+		jc.Count++
+	} else {
+		o.jobCounts[key] = &jobCount{
+			VcsType:  job.VcsType,
+			Username: job.Username,
+			Reponame: job.Reponame,
+			Branch:   job.Branch,
+			Count:    1,
+		}
+	}
+}
+
+func (o *jobCounts) toMetrics(now time.Time, metricName string) []datadog.Metric {
+	metrics := make([]datadog.Metric, len(o.jobCounts))
+	timestamp := float64(now.Unix())
+
+	i := 0
+	for _, jobCount := range o.jobCounts {
+		count := float64(jobCount.Count)
+		metrics[i] = datadog.Metric{
+			Metric: &metricName,
+			Points: []datadog.DataPoint{{&timestamp, &count}},
+			Tags: []string{
+				fmt.Sprintf("vcs_type:%s", jobCount.VcsType),
+				fmt.Sprintf("username:%s", jobCount.Username),
+				fmt.Sprintf("reponame:%s", jobCount.Reponame),
+				fmt.Sprintf("branch:%s", jobCount.Branch),
+			},
+		}
+		i++
+	}
+
+	return metrics
+}
+
+func (o *jobCounts) getTotalCount() int {
+	cnt := 0
+
+	for _, jobCount := range o.jobCounts {
+		cnt += jobCount.Count
+	}
+
+	return cnt
+}
+
+type circleCiJob struct {
+	VcsType   string `json:"vcs_type"`
+	Username  string `json:"username"`
+	Reponame  string `json:"reponame"`
+	Branch    string `json:"branch"`
+	LifeCycle string `json:"lifecycle"`
+}
+
 func main() {
 	for {
 		go getAndSendMetrics()
-		time.Sleep(15 * time.Second)
+		time.Sleep(60 * time.Second)
 	}
 }
 
 func getAndSendMetrics() {
 	now := time.Now()
-	timestamp := float64(now.Unix())
-	if running, notRunning, err := getQueueNumbers(); err != nil {
+	if runningCounts, notRunningCounts, err := getJobCounts(); err != nil {
 		log.Println(err)
 	} else {
-		log.Printf("running:%d\tnot_running:%d\n", int(running), int(notRunning))
+		log.Printf("running:%d\tnot_running:%d\n", runningCounts.getTotalCount(), notRunningCounts.getTotalCount())
 
-		metrics := []datadog.Metric{
-			datadog.Metric{
-				Metric: &runningMetricName,
-				Points: []datadog.DataPoint{{&timestamp, &running}},
-			},
-			datadog.Metric{
-				Metric: &notRunningMetricName,
-				Points: []datadog.DataPoint{{&timestamp, &notRunning}},
-			},
-		}
+		metrics := append(runningCounts.toMetrics(now, runningMetricName), notRunningCounts.toMetrics(now, notRunningMetricName)...)
+
 		if err := datadogClient.PostMetrics(metrics); err != nil {
 			log.Printf("failed to post metrics to Datadog: %s\n", err)
 		} else {
@@ -49,43 +114,48 @@ func getAndSendMetrics() {
 	}
 }
 
-func getQueueNumbers() (float64, float64, error) {
+func getJobCounts() (*jobCounts, *jobCounts, error) {
+	runningCounts := newJobCounts()
+	notRunningCounts := newJobCounts()
+
 	req, reqErr := http.NewRequest("GET", "https://circleci.com/api/v1.1/recent-builds?limit=100&circle-token="+os.Getenv("CIRCLECI_API_TOKEN"), nil)
 	if reqErr != nil {
-		return -1, -1, fmt.Errorf("failed to build HTTP request to CircleCI API: %s", reqErr)
+		return runningCounts, notRunningCounts, fmt.Errorf("failed to build HTTP request to CircleCI API: %s", reqErr)
 	}
 
 	req.Header.Add("Accept", "application/json")
 	res, resErr := http.DefaultClient.Do(req)
 	if resErr != nil {
-		return -1, -1, fmt.Errorf("failed to get recent builds from CircleCI API: %s", resErr)
+		return runningCounts, notRunningCounts, fmt.Errorf("failed to get recent builds from CircleCI API: %s", resErr)
 	}
 
 	defer res.Body.Close()
 
-	var running float64
-	var notRunning float64
-
 	d := json.NewDecoder(res.Body)
 	for {
-		var j []map[string]interface{}
-		if err := d.Decode(&j); err != nil {
+		var jobs []*circleCiJob
+		if err := d.Decode(&jobs); err != nil {
 			if err == io.EOF {
 				break
 			} else {
-				return -1, -1, fmt.Errorf("failed to parse response from CircleCI API: %s", err)
+				return runningCounts, notRunningCounts, fmt.Errorf("failed to parse response from CircleCI API: %s", err)
 			}
 		}
 
-		for _, job := range j {
-			if job["lifecycle"] == "running" {
-				running++
-			} else if job["lifecycle"] == "not_running" {
-				notRunning++
-			}
+		incrJobCounts(jobs, runningCounts, notRunningCounts)
+	}
 
+	return runningCounts, notRunningCounts, nil
+}
+
+func incrJobCounts(jobs []*circleCiJob, runningCounts, notRunningCounts *jobCounts) (*jobCounts, *jobCounts) {
+	for _, job := range jobs {
+		if job.LifeCycle == "running" {
+			runningCounts.incr(job)
+		} else if job.LifeCycle == "not_running" {
+			notRunningCounts.incr(job)
 		}
 	}
 
-	return running, notRunning, nil
+	return runningCounts, notRunningCounts
 }
